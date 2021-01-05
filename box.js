25662340535bb9f57f21d8d18a44960a0251dba6
RitishaMathur <73174195+RitishaMathur@users.noreply.github.com>
2021-01-05 20:24:20 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        //isStatic:true,
+      'restitution':0.08,
+        'friction':0.04,
+        'density':0.01
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("lightgreen");
+    
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
+
